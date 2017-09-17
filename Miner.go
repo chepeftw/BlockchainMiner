35c7b163c2f7858d8bdafa6b46de7a2c776a99f5
Jose Alfredo Alvarez Aldana <josealfredo1515@gmail.com>
2017-09-17 13:09:46 +0200

    Debugging.

--- Miner.go
+++ Miner.go
@@ -183,7 +183,7 @@ func attendMiningChannel() {
 
 									//}
 
-									log.Debug("Mining WIN => " + cryptoPuzzle)
+									log.Debug("Mining_WIN => " + cryptoPuzzle)
 									log.Debug("Checksum => " + hex.EncodeToString( checksum ) )
 									log.Debug("Checksum len => " + strconv.Itoa( len(string(checksum)) ) )
 									log.Debug("unverifiedBlocks => " + unverifiedBlocks.String())
